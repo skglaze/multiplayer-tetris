fbdffd34358c7488e7d10c7e68105fb6119056f9
Sam Glaze <samglaze@Jimmys-MacBook-Pro.local>
2019-10-02 12:38:24 -0400

    made a board and added a square to it

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const board = document.getElementById('board')
+for (i = 0; i < 200; i++) {
+    const newSquares = document.createElement('div')
+    newSquares.className = "square"
+    board.appendChild(newSquares)
+}
+
+const boardArr = document.getElementsByClassName('square')
+boardArr[4].classList.add("occupied")
+boardArr[5].classList.add("occupied")
+boardArr[14].classList.add("occupied")
+boardArr[15].classList.add("occupied")
+
 const square = [[0, 0], [0, 1], [1, 1], [1, 0]]
-
-
