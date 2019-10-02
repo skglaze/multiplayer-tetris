7560a92608ae72e0350a10ce665d6bdf0ccecbb6
Sam Glaze <samglaze@Jimmys-MacBook-Pro.local>
2019-10-02 13:07:29 -0400

    made a square that falls and stops at the bottom

--- script.js
+++ script.js
@@ -6,9 +6,37 @@ for (i = 0; i < 200; i++) {
 }
 
 const boardArr = document.getElementsByClassName('square')
-boardArr[4].classList.add("occupied")
-boardArr[5].classList.add("occupied")
-boardArr[14].classList.add("occupied")
-boardArr[15].classList.add("occupied")
+let topRight = 4
+let topLeft = 5
+let bottomRight = 14
+let bottomLeft = 15
 
-const square = [[0, 0], [0, 1], [1, 1], [1, 0]]
+boardArr[topRight].classList.add("occupied")
+boardArr[topLeft].classList.add("occupied")
+boardArr[bottomRight].classList.add("occupied")
+boardArr[bottomLeft].classList.add("occupied")
+
+stop = () => {
+    if (bottomRight > 200)
+        clearInterval(interval)
+}
+
+fall = () => {
+    topLeft = topLeft + 10
+    topRight = topRight + 10
+    bottomLeft = bottomLeft + 10
+    bottomRight = bottomRight + 10
+    stop()
+    boardArr[topRight].classList.add("occupied")
+    boardArr[topLeft].classList.add("occupied")
+    boardArr[bottomRight].classList.add("occupied")
+    boardArr[bottomLeft].classList.add("occupied")
+
+    boardArr[topRight - 10].classList.remove("occupied")
+    boardArr[topLeft - 10].classList.remove("occupied")
+}
+
+const interval = setInterval(fall, 1000)
+
+
+
