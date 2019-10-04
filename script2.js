9433173b85b92955aa26ded98e55c24682b1c7ee
Sam Glaze <samglaze@Jimmys-MacBook-Pro.local>
2019-10-04 15:03:56 -0400

    made remove rows function work

--- script2.js
+++ script2.js
@@ -92,7 +92,7 @@ const spawnBlock = () => {
 }
 
 const removeRows = () => {
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 20; i++) {
         let tempArr = []
         console.log(tempArr.length)
         for (let j = 0; j < 10; j++) {
